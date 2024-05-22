67921cd0cb968c7f67493c115c84467e0433149c
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2024-05-22 23:29:54 +0100

    console.log executes when clicked but image does not show

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-//const toads = document.querySelectorAll(".toad");
-const toads = document.getElementsByClassName("toad");
+const toads = document.querySelectorAll(".toad");
+const toadContainers = document.getElementsByClassName("grid-item");
 
+//Think its not working cause wanting to click on image rather than container
 
-function displayToad(){
-    toads.classList.add("display-toad");
+function displayToad(event){
+    //Event.currentTarget gets the specific .grid-item clicked
+    const clickedContainer = event.currentTarget;
+    clickedContainer.classList.add("display-toad");
+    //Need to add the image back in display-toad
+    console.log("It's working!");
 }
 
+/*
 function hideToad(){
-    toads.style.visibility = "hidden";
+    toad.style.visibility = "hidden";
 }
+*/
 
 
-
-toads.forEach(toad => toad.addEventListener('click', hideToad));
+//For each toad position, when clicked function displayToad will execute.
+//Array.from(toadContainers) converts the HTMLCollection to an array so forEach can be executed
+Array.from(toadContainers).forEach((toad) => {
+    toad.addEventListener('click', displayToad);
+});
 
 
 
