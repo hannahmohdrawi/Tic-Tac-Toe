6ed9b29c8446aa44a04a31f7e527c559487da23b
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2024-06-11 23:04:33 +0100

    currently onclick all toads show, needs fixed

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const toads = document.querySelectorAll(".toad");
-const toadContainers = document.getElementsByClassName("grid-item");
+const toadContainers = document.getElementsByClassName("grid-item x");
 
 //Think its not working cause wanting to click on image rather than container
 
+/*
 function displayToad(event){
     //Event.currentTarget gets the specific .grid-item clicked
     const clickedContainer = event.currentTarget;
+    
     clickedContainer.classList.add("display-toad");
     //Need to add the image back in display-toad
+    
+    
+    //toadContainers.classList.add("display-toad")
     console.log("It's working!");
 }
+*/
 
 /*
 function hideToad(){
@@ -17,12 +23,21 @@ function hideToad(){
 }
 */
 
+function displayToad(){
+    toads.forEach((toad) => {
+        toad.classList.add("display-toad");
+    })
+    
+}
 
+/*
 //For each toad position, when clicked function displayToad will execute.
 //Array.from(toadContainers) converts the HTMLCollection to an array so forEach can be executed
 Array.from(toadContainers).forEach((toad) => {
     toad.addEventListener('click', displayToad);
 });
+
+*/
 
 
 
