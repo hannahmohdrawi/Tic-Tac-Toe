3d0e8b25957288c29f7715603e09247c2bea74b9
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2024-06-18 22:29:33 +0100

    revamp of javascript file

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
 const toads = document.querySelectorAll(".toad");
 const toadContainers = document.getElementsByClassName("grid-item x");
+const restartButton = document.getElementById("restartBtn");
+const winningMessage = document.getElementsByClassName("winning-message");
+const statusMessage = document.getElementsByClassName("status-message");
+
+const winningConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+let currentPlayer = "red"
+let running = false;
+
+function startGame(){
+    toads.forEach((toad) => {
+        toad.addEventListener("click", displayToad());
+    })
+    restartButton.addEventListener("click", restartGame)
+    winningMessage.textContent = `${currentPlayers's}`
+}
+
+function displayToad(){
+    toads.forEach((toad) => {
+        toad.classList.add("display-toad");
+    })
+    
+}
+
+function updateCell(cell, index){
+
+}
+
+function changePlayer(){
+
+}
+
+function winner(){
+
+}
+
+function restartGame(){
+
+}
+
+
+
+
+
+
+
+
+
 
 //Think its not working cause wanting to click on image rather than container
 
@@ -23,12 +79,6 @@ function hideToad(){
 }
 */
 
-function displayToad(){
-    toads.forEach((toad) => {
-        toad.classList.add("display-toad");
-    })
-    
-}
 
 /*
 //For each toad position, when clicked function displayToad will execute.
