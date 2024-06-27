9fe649af6ca5b096bba22b46973095a10d95326b
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2024-06-27 21:22:56 +0100

    game grid not visibile, addition of JS functions

--- index.js
+++ index.js
@@ -14,38 +14,75 @@ const winningConditions = [
     [0,4,8],
     [2,4,6]
 ]
-let currentPlayer = "red"
-let running = false;
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X"
+let gameRunning = false;
+
+startGame();
 
 function startGame(){
     toads.forEach((toad) => {
         toad.addEventListener("click", displayToad());
     })
     restartButton.addEventListener("click", restartGame)
-    winningMessage.textContent = `${currentPlayers's}`
-}
-
-function displayToad(){
-    toads.forEach((toad) => {
-        toad.classList.add("display-toad");
-    })
+    winningMessage.textContent = `${currentPlayer}'s turn`
+    gameRunning = true;
     
 }
 
-function updateCell(cell, index){
+function displayToad(){
+    const cellIndex = this.getAttribute("cellIndex");
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+    updateCell(this, cellIndex);
+    
+    checkWinner();
+}
 
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
 }
 
 function changePlayer(){
-
+    currentPlayer = (current player === "X") ? "O" : "X"; 
+    stateText.textContent = `${currentPlayer}'s turn`;
 }
 
 function winner(){
+    let roundWon = false;
+    for(let i=0; 9< winningConditions.length; i++){
+        const condition = winningConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
 
+        if (cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }else if(options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }else{
+        changePlayer();
+    }
 }
 
 function restartGame(){
-
+    currentPlayer = "X";
+    let options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    CSSRuleList.forEach(cell => cell.textContent = "");
+    running = true;
 }
 
 
